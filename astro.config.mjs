d7db09a1a857127bfef745f6388114f3486d27f9
maquinoted <mario@todoesdigital.com>
2026-02-24 14:19:32 -0600

    fix: simplificar configuracion de sitemap y build format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'; // Esta es la que busca Astro
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  // 1. Dominio principal sin WWW y sin diagonal al final
   site: 'https://coworkguatemala.com',
-  
-  // 2. Forzamos la eliminación de la diagonal al final en todas las rutas
   trailingSlash: 'never',
-  
-  // 3. Formato de directorio para URLs limpias en Vercel
   build: {
     format: 'directory'
   },
-  
-  integrations: [
-    tailwind(), 
-    sitemap() // Lo dejamos simple para que Astro use los valores por defecto del 'site'
-  ],
+  integrations: [tailwind(), sitemap()],
 });
